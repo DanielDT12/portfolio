e7efa463a8b1f5a844e69a7b5595b51f7cbeecb9
DanielDT12 <daniel.digitaletalenter@gmail.com>
2024-05-23 19:58:55 +0200

    lagt til dark light mode funksjonalitet

--- script/script.js
+++ script/script.js
@@ -2,7 +2,37 @@ const contactBtn = document.querySelector("#contact-btn");
 const goBack = document.querySelector("#go-back");
 const formClipPath = document.querySelector(".form-wrapper");
 
+const rootElement = document.documentElement;
+const darkToggle = document.querySelector("#dark");
+const lightToggle = document.querySelector("#light");
 
+
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    darkToggle.checked = true;
+    lightToggle.checked = false;
+    rootElement.setAttribute("data-color-theme", "dark");
+} else {
+    lightToggle.checked = true;
+    darkToggle.checked = false;
+    rootElement.setAttribute("data-color-theme", "light");
+}
+
+
+darkToggle.addEventListener('click', () => {
+    rootElement.setAttribute("data-color-theme", "dark");
+    darkToggle.checked = true;
+})
+
+lightToggle.addEventListener('click', () => {
+    rootElement.setAttribute("data-color-theme", "light");
+    lightToggle.checked = true;
+})
+
+
+
+
+// toggle for clip path animation
 contactBtn.addEventListener('click', () => {
     formClipPath.classList.replace("clip-path-animation", "clip-path-animation-reverse");
 })
